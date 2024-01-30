5bc5d2519f68f53ebf6b5ba9043cf5e060b774aa
calebhanssen <calebhanssen@gmaill.com>
2024-01-30 09:33:45 -0700

    yay

--- Program.cs
+++ Program.cs
@@ -1,7 +1,29 @@
-﻿internal class Program
+﻿using System;
+
+class Program
 {
-    private static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        //Welcome the User to the Game
+        Console.WriteLine("Welcome to Tic-Tac-Toe!");
+
+        //Create a game *Board Array* to store players choices
+        TicTacToeGame game = new TicTacToeGame();
+
+        //Run through the game 
+        for (int turnCount = 0; turnCount < 9 && !game.IsGameOver(); turnCount++)
+        {
+            game.PrintBoard();  //print the board by calling method in supporting class
+            game.TakeTurn();
+            if (game.CheckForWinner())  //check for winner by using suporting class method
+                break;
+        }
+
+        //Game Over Message
+        Console.WriteLine("Game Over. Thank you for playing!");
     }
 }
+
+
+// How do we want to tell each player its their turn?
+// should we use a for loop or while loop?
